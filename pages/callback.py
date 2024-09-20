77ee51b03121e4d4d284269743ee1139e4b7b236
nsimm11 <nsimms@savronsolutions.com>
2024-09-20 12:04:55 -0400

    Working Player with Pictures

--- pages/callback.py
+++ pages/callback.py
@@ -26,15 +26,51 @@ sp_oauth = SpotifyOAuth(client_id=credentials.CLIENT_ID,
                         redirect_uri=credentials.REDIRECT_URI,
                         scope=SCOPE)
 
+def login():
+    query_params = st.query_params  # Updated from experimental_get_query_params()
+    code = query_params.get("code")
+
+    if code:
+        try:
+            #Acess Code Handling
+            if st.session_state['access_token'] == "":
+                token_info = sp_oauth.get_access_token(code)
+                if isinstance(token_info, str):
+                    access_token = token_info
+                else:
+                    access_token = token_info['access_token']
+                st.session_state['access_token'] = access_token
+                st.session_state["access_token_endTime"] = datetime.datetime.fromtimestamp(token_info['expires_at']) - datetime.timedelta(minutes=3)
+                st.toast(f"You are now authenticated!, expires at {st.session_state["access_token_endTime"]}")
+            
+            elif st.session_state['access_token'] != "" and datetime.datetime.now() > st.session_state["access_token_endTime"]:
+                st.warning("expiring token about to expire")
+                token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+                st.session_state['access_token'] = access_token
+                st.session_state["access_token_endTime"] = datetime.datetime.fromtimestamp(token_info['expires_at']) - datetime.timedelta(minutes=3)
+                st.toast(f"Token Updated!, expires at {st.session_state["access_token_endTime"]}")
+            
+            else:
+                access_token = st.session_state['access_token']
+                st.toast("Using Old Access Token")
+            
+            return access_token
+        
+        except Exception as e:
+            st.error(f"Error fetching the token: {e}")
+    else:
+        st.error("Authorization code not found in URL. Please try again.")
+
+
 def submitRequest(endpoint, headers, functionName):
 
     response = requests.get(endpoint, headers=headers)
     # Check if the response is successful
     if response.status_code == 200:
-        current_track = response.json()
+        current_response = response.json()
     
-    if current_track is not None:
-        return current_track["item"]
+    if current_response is not None:
+        return current_response["item"]
     
     return None
 
@@ -47,45 +83,33 @@ def getUserCurrentSongPlaying(access_token):
     requestsAsJson = submitRequest(endpoint, headers, "Get Users Current Playing Song")
 
     if requestsAsJson is not None:
-        st.write(requestsAsJson)
+        requestsAsDict = {}
+        requestsAsDict["SongName"] = requestsAsJson["name"]
+        requestsAsDict["ArtistsName"] = requestsAsJson["artists"][0]["name"]
+        requestsAsDict["AlbumName"] = requestsAsJson["album"]["name"]
+        requestsAsDict["AlbumArtHMTL"] = requestsAsJson['album']['images'][0]['url']
 
-    #return requestsAsDataframe
+    return requestsAsDict
+
+st.title("Simmplify")
+
+player = st.empty()
 
 # Streamlit app - callback page
 def callback_page():
-    st.title("OAuth Callback")
-    
-    # Step 2: Detect the authorization code from the URL query parameters
-    query_params = st.query_params  # Updated from experimental_get_query_params()
-    code = query_params.get("code")
-
-    if code:
-        try:
-            #Acess Code Handling
-            if st.session_state['access_token'] == "":
-                token_info = sp_oauth.get_access_token(code)
-                st.write(token_info)
-                if isinstance(token_info, str):
-                    access_token = token_info
-                else:
-                    access_token = token_info['access_token']
-                st.session_state['access_token'] = access_token
-                st.session_state["access_token_endTime"] = datetime.datetime.fromtimestamp(token_info['expires_at']) - datetime.timedelta(minutes=3)
-                st.toast(f"You are now authenticated!, expires at {st.session_state["access_token_endTime"]}")
-            
-            else:
-                access_token = st.session_state['access_token']
-                st.toast("Using Old Access Token")
-        except Exception as e:
-            st.error(f"Error fetching the token: {e}")
-    else:
-        st.error("Authorization code not found in URL. Please try again.")
 
     while True:
-        if datetime.datetime.now() > st.session_state["access_token_endTime"]:
-            st.warning("Code about to expire")
-        getUserCurrentSongPlaying(access_token)
+        access_token = login()
+        getUserCurrentSongPlayingDict = getUserCurrentSongPlaying(access_token)
+
+        with player.container():
+            st.markdown(f'SONG: {getUserCurrentSongPlayingDict["SongName"]}')
+            st.markdown(f'Artists: {getUserCurrentSongPlayingDict["ArtistsName"]}')
+            st.markdown(f'Album: {getUserCurrentSongPlayingDict["AlbumName"]}')
+            st.image(getUserCurrentSongPlayingDict["AlbumArtHMTL"])
+
         time.sleep(10)
+
 
 
 if __name__ == "__main__":
