9df70429ececd0de2895bbee55b57a4a32046095
nsimm11 <nsimms@savronsolutions.com>
2024-09-20 13:58:07 -0400

    50 historical songs in dataframe

--- pages/callback.py
+++ pages/callback.py
@@ -6,6 +6,7 @@ import requests
 import certifi
 import datetime
 import pandas as pd
+from pandas import json_normalize
 import time
 
 import credentials
@@ -46,15 +47,12 @@ def login():
             elif st.session_state['access_token'] != "" and datetime.datetime.now() > st.session_state["access_token_endTime"]:
                 st.warning("expiring token about to expire")
                 token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
-                st.session_state['access_token'] = access_token
+                st.session_state['access_token'] = token_info['access_token']
                 st.session_state["access_token_endTime"] = datetime.datetime.fromtimestamp(token_info['expires_at']) - datetime.timedelta(minutes=3)
                 st.toast(f"Token Updated!, expires at {st.session_state["access_token_endTime"]}")
             
             else:
-                access_token = st.session_state['access_token']
                 st.toast("Using Cached Access Token")
-            
-            return access_token
         
         except Exception as e:
             st.error(f"Error fetching the token: {e}")
@@ -62,25 +60,31 @@ def login():
         st.error("Authorization code not found in URL. Please try again.")
 
 
-def submitRequest(endpoint, headers, functionName):
+def submitRequest(endpoint, functionName, params):
 
-    response = requests.get(endpoint, headers=headers)
+    # Define the endpoint for currently playing track
+    headers = {
+    "Authorization": f"Bearer {st.session_state["access_token"]}"}   
+
+    response = requests.get(endpoint, headers=headers, params=params)
+
     # Check if the response is successful
     if response.status_code == 200:
         current_response = response.json()
+    else:
+        st.write(f"error, {functionName}, {response.status_code}")
+        current_response = None
     
     if current_response is not None:
         return current_response
     
     return None
 
-def getUserCurrentSongPlaying(access_token):
-    # Define the endpoint for currently playing track
-    headers = {
-    "Authorization": f"Bearer {access_token}"}   
+def getUserCurrentSongPlaying():
 
-    requestsAsJsonPlayer = submitRequest("https://api.spotify.com/v1/me/player", headers, "Get Users PlaybackState")
+    requestsAsJsonPlayer = submitRequest("https://api.spotify.com/v1/me/player", "Get Users PlaybackState", {})
     requestsAsJsonSong = requestsAsJsonPlayer["item"]
+
 
     requestsAsDict = {}
 
@@ -96,9 +100,35 @@ def getUserCurrentSongPlaying(access_token):
 
     return requestsAsDict
 
+# Function to extract 'name' from each JSON object in the array
+def extract_item(json_array, key='name'):
+    return [item[key] for item in json_array if key in item]
+
+def getHistoricalListening():
+
+    # Define the endpoint for currently playing track
+    headers = {
+    "Authorization": f"Bearer {st.session_state["access_token"]}"}   
+
+    params = {"limit": 50}
+
+    requestsAsJsonHistory = submitRequest("https://api.spotify.com/v1/me/player/recently-played", "Get Users History", params)
+
+    if requestsAsJsonHistory is not None:
+        userHistory = json_normalize(requestsAsJsonHistory["items"])
+        userHistory = userHistory[["played_at", "track.duration_ms", "track.id", "track.is_local", "track.name", "track.artists"]]
+        userHistory['artists'] = userHistory['track.artists'].apply(extract_item)
+        userHistory.drop("track.artists", axis=1, inplace=True)
+        # Display the result
+        st.dataframe(userHistory, use_container_width=True)
+    else:
+        st.write("Issue with History Request")
+
 st.title("Simmplify")
 
 player = st.empty()
+
+getHistoricalListening()
 
 # Streamlit app - callback page
 def callback_page():
@@ -107,8 +137,8 @@ def callback_page():
 
     while True:
         if (startTime - datetime.datetime.now()).total_seconds() % 10 < 1:
-            access_token = login()
-            getUserCurrentSongPlayingDict = getUserCurrentSongPlaying(access_token)
+            login()
+            getUserCurrentSongPlayingDict = getUserCurrentSongPlaying()
 
         else: getUserCurrentSongPlayingDict["SongCurrentPosition"] = min(float(getUserCurrentSongPlayingDict["SongLength"]), float(getUserCurrentSongPlayingDict["SongCurrentPosition"]) + 1000)
 
@@ -120,7 +150,6 @@ def callback_page():
             st.image(getUserCurrentSongPlayingDict["AlbumArtHMTL"])
 
         time.sleep(1)
-
 
 
 if __name__ == "__main__":
