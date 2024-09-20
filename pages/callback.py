e2196eeae32a15286cfcdec3be6903d60f9e2840
nsimm11 <nsimms@savronsolutions.com>
2024-09-20 11:29:04 -0400

    Improving sign in to reference saved token, loop that checks if token is expiring

--- pages/callback.py
+++ pages/callback.py
@@ -2,12 +2,21 @@ import streamlit as st
 import spotipy as sp
 from spotipy.oauth2 import SpotifyOAuth
 import os
+import requests
 import certifi
+import datetime
+import pandas as pd
+import time
 
 import credentials
 
 # Use certifi's certificate bundle
 os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
+
+if 'access_token' not in st.session_state:
+    st.session_state['access_token'] = ''
+if 'access_token_endTime' not in st.session_state:
+    st.session_state['access_token_endTime'] = ''
 
 # Scopes required for accessing user's currently playing track
 SCOPE = "user-read-playback-state user-read-currently-playing"
@@ -16,6 +25,31 @@ sp_oauth = SpotifyOAuth(client_id=credentials.CLIENT_ID,
                         client_secret=credentials.CLIENT_SECRET,
                         redirect_uri=credentials.REDIRECT_URI,
                         scope=SCOPE)
+
+def submitRequest(endpoint, headers, functionName):
+
+    response = requests.get(endpoint, headers=headers)
+    # Check if the response is successful
+    if response.status_code == 200:
+        current_track = response.json()
+    
+    if current_track is not None:
+        return current_track["item"]
+    
+    return None
+
+def getUserCurrentSongPlaying(access_token):
+    # Define the endpoint for currently playing track
+    endpoint = "https://api.spotify.com/v1/me/player/currently-playing"
+    headers = {
+    "Authorization": f"Bearer {access_token}"}   
+
+    requestsAsJson = submitRequest(endpoint, headers, "Get Users Current Playing Song")
+
+    if requestsAsJson is not None:
+        st.write(requestsAsJson)
+
+    #return requestsAsDataframe
 
 # Streamlit app - callback page
 def callback_page():
@@ -27,31 +61,32 @@ def callback_page():
 
     if code:
         try:
-            token_info = sp_oauth.get_access_token(code)
-
-            st.write(token_info)
-
-            # Adjust code to handle the future return type (token string) vs. current (dictionary)
-            if isinstance(token_info, str):
-                # Future behavior (token string)
-                access_token = token_info
+            #Acess Code Handling
+            if st.session_state['access_token'] == "":
+                token_info = sp_oauth.get_access_token(code)
+                st.write(token_info)
+                if isinstance(token_info, str):
+                    access_token = token_info
+                else:
+                    access_token = token_info['access_token']
+                st.session_state['access_token'] = access_token
+                st.session_state["access_token_endTime"] = datetime.datetime.fromtimestamp(token_info['expires_at']) - datetime.timedelta(minutes=3)
+                st.toast(f"You are now authenticated!, expires at {st.session_state["access_token_endTime"]}")
+            
             else:
-                # Current behavior (token dictionary)
-                access_token = token_info['access_token']
-
-            # Store access token in session_state
-            st.session_state['access_token'] = access_token
-
-            st.toast("You are now authenticated!")
-            results = sp.current_user_saved_tracks()
-            for idx, item in enumerate(results['items']):
-                track = item['track']
-                st.write(idx, track['artists'][0]['name'], " – ", track['name'])
-
+                access_token = st.session_state['access_token']
+                st.toast("Using Old Access Token")
         except Exception as e:
             st.error(f"Error fetching the token: {e}")
     else:
         st.error("Authorization code not found in URL. Please try again.")
+
+    while True:
+        if datetime.datetime.now() > st.session_state["access_token_endTime"]:
+            st.warning("Code about to expire")
+        getUserCurrentSongPlaying(access_token)
+        time.sleep(10)
+
 
 if __name__ == "__main__":
     callback_page()
