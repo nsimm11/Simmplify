b83633ca2c68941057061ffc3f7ea3c8fa660cdd
nsimm11 <nsimms@savronsolutions.com>
2025-01-02 17:34:38 -0500

    reload trouble

--- frontend/callback.py
+++ frontend/callback.py
@@ -117,6 +117,9 @@ def getUserId():
 
 def login():
 
+    st.write("Access Token: ",    st.session_state['access_token'])
+    st.write("Access Token End Time: ", st.session_state["access_token_endTime"])
+    st.write("Refresh Token: ", st.session_state["refresh_token"]) 
 
     if st.session_state['access_token'] != '' and st.session_state["access_token_endTime"] != '' and st.session_state["refresh_token"] != '':
         st.toast("You are already logged in!")
@@ -631,9 +634,7 @@ else:
 
     # Call the login function at the start of the script
 
-    st.write("Access Token: ",    st.session_state['access_token'])
-    st.write("Access Token End Time: ", st.session_state["access_token_endTime"])
-    st.write("Refresh Token: ", st.session_state["refresh_token"]) 
+
 
     login()
 
