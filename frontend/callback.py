2d553d85b4418ba8c5782a839e07c92867f14b0b
nsimm11 <nsimms@savronsolutions.com>
2025-01-02 11:15:43 -0500

    Historical Data Display

--- frontend/callback.py
+++ frontend/callback.py
@@ -306,13 +306,13 @@ def getHistoricalData(userUri):
     
 
     st.write(historicalData["listeningStartTime"].max())
-    
+
     return historicalData
 
 def format_historical_data(df, playlist_name):
     #Limit to the last 150 rows since the data will become too large to display
-    df = df.sort_values(by='listeningStartTime', ascending=False)
-    df = df.head(150)
+    #df = df.sort_values(by='listeningStartTime', ascending=False)
+    #df = df.head(150)
     
     # Add playlist name to the DataFrame
     if playlist_name != "ALL":
