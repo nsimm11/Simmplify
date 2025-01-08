c75376d091016b6a2baaec0319008a646e5ed827
nsimm11 <nsimms@savronsolutions.com>
2025-01-07 20:26:19 -0500

    post gres trouble

--- frontend/callback.py
+++ frontend/callback.py
@@ -71,6 +71,8 @@ conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
 
 cursor = conn.cursor()
 
+st.write(st.secrets)
+
 def connect_to_db_postgres():
     # Load SSH and PostgreSQL secrets
     ssh_username = st.secrets["ssh"]["username_ssh"]
