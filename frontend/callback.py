e987189fe3a3f75455d0673cf1ac73bd38474be2
nsimm11 <nsimms@savronsolutions.com>
2025-01-10 09:20:54 -0500

    fixing storeTokens insert query

--- frontend/callback.py
+++ frontend/callback.py
@@ -706,7 +706,7 @@ def display_stats(column, title, statType, display_percentage):
 
             count += 1
     else:
-        st.markdown(f"<div style='color: #FFFFFF; text-align: center;'>{title}</div>", unsafe_allow_html=True)
+        st.markdown(f"<div style='color: #FFFFFF; text-align: center;'>No songs yet! Start listening and come back soon!</div>", unsafe_allow_html=True)
 
 def clearFromSpotifyPlaylist(playlistUri, songUris):
     # Extract the playlist ID from the URI
