4f27f181a027e02111df617efa1f7e1a45883def
nsimm11 <nsimms@savronsolutions.com>
2025-01-10 11:29:56 -0500

    trying to refresh advanced stats

--- frontend/callback.py
+++ frontend/callback.py
@@ -245,7 +245,9 @@ def exchange_code_for_token(auth_code):
         st.session_state["access_token_endTime"] = datetime.now(pytz.utc) + timedelta(seconds=token_info['expires_in'])
         st.session_state["refresh_token"] = token_info['refresh_token']
         requestsAsJsonUser = submitRequest("https://api.spotify.com/v1/me", "Get Users PlaybackState", {})
-        st.session_state["UserUri"] = requestsAsJsonUser["uri"]
+        st.write(requestsAsJsonUser)
+        if "uri" in requestsAsJsonUser:
+            st.session_state["UserUri"] = requestsAsJsonUser["uri"]
         st.toast(f"You are now authenticated!, expires at {st.session_state['access_token_endTime']}")
     else:
         st.toast(f"Error fetching the token: {response.status_code} - {response.text}")
