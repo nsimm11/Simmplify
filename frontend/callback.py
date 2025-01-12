1d624b283e418c7e8c85d2597a41d54afb54af69
nsimm11 <nsimms@savronsolutions.com>
2025-01-12 12:55:10 -0500

    troubleshooting database connection

--- frontend/callback.py
+++ frontend/callback.py
@@ -112,6 +112,7 @@ def connect_to_db_postgres():
             return conn, cursor
         except Exception as error:
             st.warning(f"An exception occurred: {error}")
+            return "",""
 
     except Exception as error:
         st.write("An exception occurred:", error)
