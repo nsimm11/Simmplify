14c62072fce2c67db4acb58b33bcc2a31891dba0
nsimm11 <nsimms@savronsolutions.com>
2025-01-14 22:28:39 -0500

    fixing connection issues - adding port string

--- frontend/callback.py
+++ frontend/callback.py
@@ -490,6 +490,7 @@ def getHistoricalData(userUri, userPlaylists):
         lambda x: x.replace(tzinfo=pytz.utc).astimezone(user_timezone).replace(microsecond=0)
     )
 
+    st.write(userPlaylists)
     playlist_map = userPlaylists.set_index('uri')['name'].to_dict()
     historicalData['Playlist Name'] = historicalData['playlisturi'].map(playlist_map).fillna("Non-User Playlist")
 
